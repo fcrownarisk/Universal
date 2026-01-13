4fdf636de518d86f70e8b464029d3d6251e24a0e
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2026-01-13 15:05:54 +0800

    Update time.ts

--- SpaceTime/Time/time.ts
+++ SpaceTime/Time/time.ts
@@ -6,5 +6,5 @@ import "t7t8.ts"
 import "t9t10.ts"
 import "t11t0.ts"
 export function time(){
-  '(t1 - t2)^2' + '(t3 - t4)^2' + '(t5 - t6)^2' == '(t7 - t8)^2' + '(t9 - t10)^2'+ '(t11 - t0)^2'
+  '(t1 - t2)^2' + '(t3 - t4)^2' + '(t5 - t6)^2' == '(t7 - t8)^2' + '(t9 - t10)^2' + '(t11 - t0)^2'
 }
